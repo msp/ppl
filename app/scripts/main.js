efe3c87ac39d8a9b93a73465335d0a1b255aec49
msp <matt@cenatus.org>
2015-02-16 17:53:53 +0000

    New SVG file imprted and augmented with groups. Store Webcode editor .pcvd

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,39 @@
 /* jshint devel:true */
 console.log("PPL starting..");
 
-var s = Snap(1024, 768);
+var s = Snap(2000,2000);
 
 Snap.load("/ppl-pie-menu.svg", function (f) {
   // amend whilst loading..
-  var innerleft   = f.select("#innerleft");
-  var innertop    = f.select("#innertop");
-  var innerbottom = f.select("#innerbottom");
+  var innerLeft   = f.select("#innerLeft");
+  var innerTop    = f.select("#innerTop");
+  var innerBottom = f.select("#innerBottom");
 
-  var outerleft   = f.selectAll("#outerleft path").attr({visibility: "hidden"});
-  var outertop    = f.selectAll("#outertop path").attr({visibility: "hidden"});
-  var outerbottom = f.selectAll("#outerbottom path").attr({visibility: "hidden"});
+  var outerLeft   = f.selectAll("#outerLeft path").attr({visibility: "hidden"});
+  var outerTop    = f.selectAll("#outerTop path").attr({visibility: "hidden"});
+  var outerBottom = f.selectAll("#outerBottom path").attr({visibility: "hidden"});
 
   // attach to DOM
   s.append(f);
 
   // playtime
   var tl = new TimelineLite();
-  tl.staggerFrom(".innerPie", 1, { autoAlpha: 0 }, 0.2)
+  tl.staggerFrom($("#innerLeft, #innerTop, #innerBottom"), 1, { autoAlpha: 0 }, 0.2)
 
 
   // events
-  innerleft.node.onclick = function () {
-    tl.staggerFrom(".outerleft", 1, { autoAlpha: 0 }, 0.05);
+  innerLeft.node.onclick = function () {
+    // tl.staggerFrom(".outerLeft", 1, { autoAlpha: 0 }, 0.05);
+    tl.staggerFrom($("#outerLeft path"), 1, { autoAlpha: 0 }, 0.05);
   };
 
-  innertop.node.onclick = function () {
-    tl.staggerFrom(".outertop", 1, { autoAlpha: 0 }, 0.05);
+  innerTop.node.onclick = function () {
+    tl.staggerFrom($("#outerTop path"), 1, { autoAlpha: 0 }, 0.05);
   };
 
-  innerbottom.node.onclick = function () {
-    tl.staggerFrom(".outerbottom", 1, { autoAlpha: 0 }, 0.05);
+  innerBottom.node.onclick = function () {
+    tl.staggerFrom($("#outerBottom path"), 1, { autoAlpha: 0 }, 0.05);
   };
 
-  // innerleft.text(190, 80, "Top Left foo "+tr.node);
+  // innerLeft.text(190, 80, "Top Left foo "+tr.node);
 });
