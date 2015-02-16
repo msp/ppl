11ac007fbca7b4ba9e951d58358b04cf6d43eebe
msp <matt@cenatus.org>
2015-02-16 16:37:52 +0000

    GSAP for timeline & animations. Simple intro.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,51 +5,34 @@ var s = Snap(1024, 768);
 
 Snap.load("/ppl-pie-menu.svg", function (f) {
   // amend whilst loading..
-  var outerleft   = f.selectAll("#outerleft path").attr({ display: "none" });
-  var outertop    = f.selectAll("#outertop path").attr({ display: "none" });
-  var outerbottom = f.selectAll("#outerbottom path").attr({ display: "none" });
+  var innerleft   = f.select("#innerleft");
+  var innertop    = f.select("#innertop");
+  var innerbottom = f.select("#innerbottom");
+
+  var outerleft   = f.selectAll("#outerleft path").attr({visibility: "hidden"});
+  var outertop    = f.selectAll("#outertop path").attr({visibility: "hidden"});
+  var outerbottom = f.selectAll("#outerbottom path").attr({visibility: "hidden"});
 
   // attach to DOM
   s.append(f);
 
-  var innerleft   = s.select("#innerleft");
-  var innertop    = s.select("#innertop");
-  var innerbottom = s.select("#innerbottom");
+  // playtime
+  var tl = new TimelineLite();
+  tl.staggerFrom(".innerPie", 1, { autoAlpha: 0 }, 0.2)
+
 
   // events
-  innerleft.node.onmouseover = function () {
-    console.log("MSP onmouseover left");
-    outerleft.attr(({ display: "block" }));
-    // innerleft.transform("t100,100r45t-100,0")
+  innerleft.node.onclick = function () {
+    tl.staggerFrom(".outerleft", 1, { autoAlpha: 0 }, 0.05);
   };
 
-  innerleft.node.onmouseout = function () {
-    console.log("MSP onmouseout left");
-    outerleft.attr(({ display: "none" }));
+  innertop.node.onclick = function () {
+    tl.staggerFrom(".outertop", 1, { autoAlpha: 0 }, 0.05);
   };
 
-  innertop.node.onmouseover = function () {
-    console.log("MSP onmouseover top");
-    outertop.attr(({ display: "block" }));
-    // innerleft.transform("t100,100r45t-100,0")
+  innerbottom.node.onclick = function () {
+    tl.staggerFrom(".outerbottom", 1, { autoAlpha: 0 }, 0.05);
   };
 
-  innertop.node.onmouseout = function () {
-    console.log("MSP onmouseout top");
-    outertop.attr(({ display: "none" }));
-  };
-
-  innerbottom.node.onmouseover = function () {
-    console.log("MSP onmouseover bottom");
-    outerbottom.attr(({ display: "block" }));
-    // innerleft.transform("t100,100r45t-100,0")
-  };
-
-  innerbottom.node.onmouseout = function () {
-    console.log("MSP onmouseout bottom");
-    outerbottom.attr(({ display: "none" }));
-  };
-
-  // innerleft.animate({r: 150}, 1000);
   // innerleft.text(190, 80, "Top Left foo "+tr.node);
 });
